1339906b48d0fa32d4be19e1b45f502377485ef5
rupali <rupali@gmail.com>
2026-01-21 10:13:21 +0530

    version-2

--- p1.py
+++ p1.py
@@ -3,3 +3,10 @@ if a==a[::-1]:
     print("It's Palindrome")
 else:
      print("Not Palindrome")
+
+
+a1=input("Enter a word:")
+if a1==a1[::-1]:
+   print("Palindrome")
+else:
+    print("Not Palindrome")
